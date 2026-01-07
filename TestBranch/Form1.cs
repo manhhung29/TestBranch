779bfcc34de199d226027ae7453adbf65bd5f3b8
MANHHUNG <MANHHUNG@DESKTOP-9DJ08FU>
2026-01-07 14:20:53 +0700

    Feature graph test commit

--- TestBranch/Form1.cs
+++ TestBranch/Form1.cs
@@ -21,7 +21,7 @@ namespace TestBranch
             {
                 //test nè
                 //Test tiếp nè
-
+                // GRAPH TEST - FEATURE BRANCH
                 new User { Id = 1, Name = "Hung", Department = "IT" },
                 new User { Id = 2, Name = "An", Department = "QA" },
                 new User { Id = 3, Name = "Binh", Department = "IT" }
